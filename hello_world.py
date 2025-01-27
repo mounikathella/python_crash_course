7551539b6415c99408ee2ec042c5ce182c6363e9
Mounika Thella <mouni@Mounikas-MacBook-Air.local>
2025-01-27 12:21:54 -0500

    adding hello world file

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+message = "Hello Python Crash Course reader!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+name=first_name + " " + last_name
+print(name)
+print("\tHello")
